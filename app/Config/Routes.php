d4acc40b8ba5dc9576d0d7e103c23dacb81d22d3
RADON SIMBA <simbaradon100@gmail.com>
2026-02-12 11:59:55 +0300

    feat: create routes for rendering a view of Auth/login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+
+//AUTH
+$routes->get('/', 'Auth::login');
+$routes->get('/auth_login', 'Auth::login');

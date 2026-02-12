e2f3c05a7dd497bc43e26eff639d1e0b8e80973d
RADON SIMBA <simbaradon100@gmail.com>
2026-02-12 12:20:05 +0300

    feat: configuring the style of the view of Auth/login

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php echo view('Auth/components/cssFile'); ?>
+    <title>
+        <?php echo $title ?>
+    </title>
 </head>
 <body>
     <h1> i am login testing page</h1>

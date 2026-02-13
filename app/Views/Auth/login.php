55d53b688d56dbacffb97763d041b6067eaebfbd
RADON SIMBA <simbaradon100@gmail.com>
2026-02-13 09:44:50 +0300

    chore: modify the form

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -18,32 +18,30 @@
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <b-form @submit.prevent="login">
-                                <div class="text-center mb-3">
-                                    <img src="<?php echo base_url('/style/images/logo_2.png'); ?>" alt="Logo"
-                                        class="login-logo">
-                                    <h5 class="card-title text-center mb-4"> GEOTRUST</h5>
-                                </div>
-                                <hr>
-                                <h5 class="card-title text-center mb-4">Login</h5>
+                            <div class="text-center mb-3">
+                                <img src="<?php echo base_url('/style/images/logo_2.png'); ?>" alt="Logo"
+                                    class="login-logo">
+                                <h5 class="card-title text-center mb-4"> GEOTRUST</h5>
+                            </div>
+                            <hr>
+                            <h5 class="card-title text-center mb-4">Login</h5>
 
-                                <b-form-group label="Email:" label-for="emailInput">
-                                    <b-form-input id="emailInput" type="email" v-model="email"
-                                        placeholder="Enter email" required></b-form-input>
-                                </b-form-group>
+                            <label for="email"> Email</label>
+                            <b-form-input id="emailInput" type="email" v-model="email" placeholder="Enter email"
+                                required></b-form-input>
 
-                                <b-form-group label="Password:" label-for="passwordInput">
-                                    <b-form-input id="passwordInput" type="password" v-model="password"
-                                        placeholder="Enter password" required></b-form-input>
-                                </b-form-group>
 
-                                <button type="submit" class="btn login-btn btn-primary" style="margin-top: 0.5rem;width: 100%;">Login</button>
+                            <label for="password"> password</label>
+                            <b-form-input id="passwordInput" type="password" v-model="password"
+                                placeholder="Enter password" required></b-form-input>
 
-                                <p class="mt-3 text-center text-muted">
-                                    I don't have an account? <a href="#">Register</a>
-                                </p>
-                            </b-form>
 
+                            <b-button class="btn login-btn btn-primary"
+                                style="margin-top: 0.5rem;width: 100%;" @click="onPressedLogin()">Login</b-button>
+
+                            <p class="mt-3 text-center text-muted">
+                                I don't have an account? <a href="#">Register</a>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -54,7 +52,7 @@
     <?php echo view('Auth/components/jsFile'); ?>
     <?php echo view('Auth/loginFooter'); ?>
 
-    
+
 </body>
 
 </html>

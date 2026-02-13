7ffb84feab95fd61f2d17a812648b3da8af099e0
RADON SIMBA <simbaradon100@gmail.com>
2026-02-13 09:51:48 +0300

    feat: create function of constructor on Auth controller

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,17 +3,22 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Auth extends BaseController
 {
     protected $data;
+    protected $model;
+    public function __construct(){
+        $this->model = new UserModel();
+    }
     public function login()
     {
         $this->data['title'] = 'Login || GeoTrust';
         return view('Auth/login', $this->data);
     }
     public function check(){
-        
+
     }
 }

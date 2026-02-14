11a33ff6e14698b8eb22b30ab728f9e76f9940b5
RADON SIMBA <simbaradon100@gmail.com>
2026-02-14 13:28:36 +0300

    feat: add notification panel

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -24,6 +24,13 @@
                                 <h5 class="card-title text-center mb-4"> GEOTRUST</h5>
                             </div>
                             <hr>
+                            <div class="row">
+                                <div class="col-md-12 text-center">
+                                    <b-alert :show="showMessage" :variant="variant">
+                                       {{message}}
+                                    </b-alert>
+                                </div>
+                            </div>
                             <h5 class="card-title text-center mb-4">Login</h5>
 
                             <label for="email"> Email</label>
@@ -36,8 +43,8 @@
                                 placeholder="Enter password" required></b-form-input>
 
 
-                            <b-button class="btn login-btn btn-primary"
-                                style="margin-top: 0.5rem;width: 100%;" @click="onPressedLogin()">Login</b-button>
+                            <b-button class="btn login-btn btn-primary" style="margin-top: 0.5rem;width: 100%;"
+                                @click="onPressedLogin()">Login</b-button>
 
                             <p class="mt-3 text-center text-muted">
                                 I don't have an account? <a href="#">Register</a>
@@ -49,7 +56,6 @@
         </div>
     </div>
 
-    <?php echo view('Auth/components/jsFile'); ?>
     <?php echo view('Auth/loginFooter'); ?>
 
 

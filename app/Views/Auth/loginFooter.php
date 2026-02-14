6d00a5db6cd09c1f0ed918b69d0579aa69d98748
RADON SIMBA <simbaradon100@gmail.com>
2026-02-14 13:29:41 +0300

    feat: create message tag on the footer

--- app/Views/Auth/loginFooter.php
+++ app/Views/Auth/loginFooter.php
@@ -1,22 +1,38 @@
+<?php echo view('Auth/components/jsFile'); ?>
 <script>
     let app = new Vue({
         el: '#app',
         data: {
             email: '',
             password: '',
+            message: '',
+            showMessage: false,
+            variant: ''
         },
         methods: {
-            onPressedLogin(){
+            onPressedLogin() {
                 let formData = new FormData();
                 formData.append('email', this.email);
                 formData.append('password', this.password);
 
-                axios.post('/check_user', formData).then(res=>{
-                    console.log(res);
+                axios.post('/check_user', formData).then(res => {
+                    if(res.data.success){
+                        this.message = res.data.message;
+                        this.showMessage = true;
+                        this.variant = 'success';
+                        setTimeout(()=>{
+                            window.location.href = '/dashboard';
+                        }, 1000)
+                    }else{
+                        this.message = res.data.message;
+                        this.showMessage = true;
+                        this.variant = 'danger';
+                    }
                     
-                }).catch(err=>{
+
+                }).catch(err => {
                     console.error(err);
-                    
+
                 })
             }
         },
